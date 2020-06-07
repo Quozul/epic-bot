68b92f55498dce9544e86fa711f622b0ede843ae
Quozul <30729291+Quozul@users.noreply.github.com>
2020-06-07 13:25:29 +0200

    Fixes

--- events/nitro_boost.js
+++ events/nitro_boost.js
@@ -1,14 +1,16 @@
+const Discord = require('discord.js');
+
 module.exports = {
     name: 'boost',
     event: 'guildUpdate',
     description: "Affiche une barre de progression lorsque le serveur vient d'être boosté.",
     execute(oldGuild, newGuild, client) {
-        if (oldGuild.premiumSubscriptionCount == newGuild.premiumSubscriptionCount) return;
+        if (oldGuild.premiumSubscriptionCount != newGuild.premiumSubscriptionCount) return;
 
         const tiers = ['1️⃣', '2️⃣', '3️⃣'];
         let j = 0;
 
-        const count = Math.min(msg.guild.premiumSubscriptionCount, 31);
+        const count = Math.min(newGuild.premiumSubscriptionCount, 31);
 
         let str = '';
         for (let i = 0; i < 32; i++) {
@@ -30,10 +32,10 @@ module.exports = {
         const embed = new Discord.MessageEmbed()
             .setColor('#0099ff')
             .setTitle('Progression du boost nitro')
-            .setFooter(msg.author.username, msg.author.avatarURL())
+            // .setFooter(msg.author.username, msg.author.avatarURL())
             .setTimestamp()
-            .setDescription('`[' + str + ']`');
+            .setDescription('```[' + str + ']```');
 
-        newGuild.systemChannel.channel.send(embed);
+        newGuild.systemChannel.send(embed);
     }
 }
